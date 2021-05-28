40ad07f28e788ac7ee350cbd066cba3c7f613a83
akinkunmi akindele hammed <ha.akinkunmi@gmail.com>
2021-05-28 19:46:48 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,4 @@ reponames=emr-hive-dataset
 #write pr related info into a file named pr_info.json because we need the file later
 curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${owner}/${reponames}/pulls/${prnumber}/files >> pr_info.json
 git clone https://github.com/${owner}/${reponames}.git
-chmod +x scripts/pybuild.py
-python scripts/pybuild.py
+python3 scripts/pybuild.py
