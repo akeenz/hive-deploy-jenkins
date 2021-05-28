4375c353250dc86ad5bbf6fc5f31b72a2b24032e
akinkunmi akindele hammed <ha.akinkunmi@gmail.com>
2021-05-28 18:43:40 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@ mkdir artifact_folder
 owner=akeenz
 reponame=emr-data-hive
 #write pr related info into a file named pr_info.json because we need the file later
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${owner}/{${reponame}/pulls/${prnumber}/files >> pr_info.json
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${owner}/${reponame}/pulls/${prnumber}/files >> pr_info.json
 git clone https://github.com/${owner}/${reponame}.git
 python scripts/pybuild.py
