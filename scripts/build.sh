dbd60bb64c4a571a241dfd44d02b04b0be2e6791
akinkunmi akindele hammed <ha.akinkunmi@gmail.com>
2021-05-28 18:55:22 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #create directory needed
 mkdir artifact_folder
 owner=akeenz
-reponame=emr-hive-dataset
+reponames=emr-hive-dataset
 #write pr related info into a file named pr_info.json because we need the file later
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${owner}/${reponame}/pulls/${prnumber}/files >> pr_info.json
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${owner}/${reponames}/pulls/${prnumber}/files >> pr_info.json
 git clone https://github.com/${owner}/${reponame}.git
 python scripts/pybuild.py
 #clone sucesfull
