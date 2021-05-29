23df8cc5b8cf0d6836f0626c5c7db3bb06cee086
akinkunmi akindele hammed <ha.akinkunmi@gmail.com>
2021-05-29 12:12:06 +0100

    Update tablecreate.py

--- scripts/tablecreate.py
+++ scripts/tablecreate.py
@@ -1,5 +1,6 @@
 import os
 import pymysql
+import pandas as pd
 DB_HOST="chalice-test.c4pa6qyxzm5o.us-east-2.rds.amazonaws.com"
 DB_USER="admin"
 DB_PASSWORD="password"
@@ -15,7 +16,21 @@ conn = pymysql.connect(
         )
 conn.autocommit = True
 cursor = conn.cursor()
-# sql_file=open("artifact_folder/sale.sql")
-# sql_as_string=sql_file.read()
-# cursor.execute(sql_as_string)
-cursor.execute("select * from Persons")
+sql_file=open("artifact_folder/sale.sql")
+sql_as_string=sql_file.read()
+cursor.execute(sql_as_string)
+# import pandas as pd
+# import pyodbc
+# import numpy as np
+ 
+# sql_conn = 'DRIVER=SQL Server Native Client 11.0; SERVER=desktop-r9rnma8; DATABASE=pollutin_db; Trusted_Connection=yes'
+
+# sql_conn = pyodbc.connect(sql_conn)
+ 
+query="select * from Persons$"
+
+
+sale = pd.read_sql(query, cursor)
+ 
+sale.head(10)
+
