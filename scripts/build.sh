7408b25d528b7f5dc947ac0c0a8901ae074f4b27
akinkunmi akindele hammed <ha.akinkunmi@gmail.com>
2021-05-30 18:19:47 +0100

    123

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,72 @@
 mkdir artifact_folder
 owner=akeenz
 reponames=emr-hive-dataset
+#Validate PR number
+re='^[0-9]+$'
+if [[ ${prnumber} =~ $re ]] ; then
+    if ! [[ "${PR_VALIDITY}" == "null" ]] ; then
+        echo "Looks like "${PR_NUMBER}" is not a Valid PR Number for the Dataset Repo"
+        exit -1
+    fi
+else
+    echo "Enter a Numeric Value for PR_NUMBER"
+    exit -1
+fi 
 #write pr related info into a file named pr_info.json because we need the file later
 curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${owner}/${reponames}/pulls/${prnumber}/files > pr_info.json
-pr_property=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${owner}/${reponames}/pulls/${prnumber})
-pr_branch_name=$(echo ${pr_property} | jq .head.ref | tr -d '"')
+PR_PROPERTY=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${owner}/${reponames}/pulls/${prnumber})
+PR_BRANCH_NAME=$(echo $PR_PROPERTY | jq .head.ref | tr -d '"')
+#change into the directory of the dataset repo [It must have been cloned at the inital stage when job was created]
+echo "I will be cloning the dataset repo"
 git clone https://github.com/${owner}/${reponames}.git
+cd ${reponames}
+git checkout ${PR_BRANCH_NAME}
+echo "we are in ${PR_BRANCH-NAME} branch "
+
+
+PR_BRANCH_DESTINATION=$(echo $PR_PROPERTY | jq .base.ref | tr -d '"')
+IS_MERGED=$(echo $PR_PROPERTY | jq .merged | tr -d '"')
+DEPLOYMENT_ENV=os.getenv('DEPLOYMENT_ENV')
+#if this is a prod deployment, check if the PR has been merged into master
+echo "Deploying into ${DEPLOYMENT_ENV}"
+echo "This PR is Merging from ${PR_BRANCH_NAME} into ${PR_BRANCH_DESTINATION}"
+echo "Is Code Merged already? And: ${IS_MERGED}"
+
+if [[ "${DEPLOYMENT_ENV}" == "prod" ]]; then
+    if [[ "${PR_BRANCH_DESTINATION}" == "master" ]]; then
+        if [[ "${IS_MERGED}" == "false" ]]; then
+            echo "I cannot Deploy your Code into Prod until it is has successfully merged into the Master Branch"
+            exit -1
+        fi
+    else
+        echo "To Deploy into Prod,the PR must be attempting to Merge into master and not ${PR_BRANCH_DESTINATION}"
+        exit -1
+    fi
+else
+    echo "This will be a Non Prod Deployment"
+fi
+
+#checkout the PR branch for
+if [[ "${DEPLOYMENT_ENV}" == "prod" ]]; then
+    git checkout $PR_BRANCH_DESTINATION
+    echo "Currently checked into ${PR_BRANCH_DESTINATION} branch"
+else
+    git checkout $PR_BRANCH_NAME
+    echo "Currently checked into ${PR_BRANCH_NAME} branch"
+fi
+
+
+
+
+
+#change into the directory of the dataset repo [It must have been cloned at the inital stage when job was created]
+echo "I will be cloning the dataset repo"
+git clone https://${GITHUB_TOKEN_USR}:${GITHUB_TOKEN_PSW}@github.intuit.com/golokunwolu/${dataset_dir}.git
+cd ${dataset_dir}
+
+
+
+
 cd ${reponames}
 git checkout ${pr_branch_name}
 echo "we are in ${pr_branch_name} branch "
--- scripts/tablecreate.py
+++ scripts/tablecreate.py
@@ -1,11 +1,11 @@
 import os
 import pymysql
 import pandas as pd
-env = os.getenv('deployment_env')
+DEPLOYMENT_ENV=os.getenv('DEPLOYMENT_ENV')
 DB_HOST="chalice-test.c4pa6qyxzm5o.us-east-2.rds.amazonaws.com"
 DB_USER="admin"
 DB_PASSWORD="password"
-DB_NAME="chalice_db_" + env
+DB_NAME="chalice_db_" + DEPLOYMENT_ENV
 conn = pymysql.connect(
         host=DB_HOST,
         user=DB_USER,
@@ -14,84 +14,48 @@ conn = pymysql.connect(
         )
 conn.autocommit = True
 cursor = conn.cursor()
-dataset_dir=cg_dataset_test 
-REPO_PULL=https://github.intuit.com/api/v3/repos/golokunwolu/${dataset_dir}/pulls/
-PR_PROPERTY=$(curl -u "${GITHUB_TOKEN_USR}:$GITHUB_TOKEN_PSW" -H "Accept: application/vnd.github.v3+json" ${REPO_PULL}${PR_NUMBER})
-PR_VALIDITY=$(echo $PR_PROPERTY | jq .message | tr -d '"')
-DATASET_CLONE_LINK = "https://${GITHUB_TOKEN_USR}:${GITHUB_TOKEN_PSW}@github.intuit.com/golokunwolu/${dataset_dir}.git"
 
 
 
 
-# #Delete files.folders if they exist
-# if [ -f pr_${PR_NUMBER}_pull_info.json ]; then
-#     rm -rf pr_${PR_NUMBER}_pull_info.json
-#     echo "Deleting pr_${PR_NUMBER}_pull_info.json ..."
-# fi
-# if [ -d pr_${PR_NUMBER}_related_objects ]; then
-#     rm -rf pr_${PR_NUMBER}_related_objects
-#     echo "Deleting pr_${PR_NUMBER}_related_objects ..."
-# fi
 
-# if [ -f pr_${PR_NUMBER}_db_object_grant.hql ]; then
-#     rm -rf pr_${PR_NUMBER}_db_object_grant.hql
-#     echo "Deleting pr_${PR_NUMBER}_db_object_grant.hql ..."
-# fi
-# if [ -d ${dataset_dir} ]; then
-#     echo "Deleting ${dataset_dir} ..."
-#     rm -rf ${dataset_dir}
-# fi
 
-# #Create files and directories needed for deployment
-# touch pr_${PR_NUMBER}_pull_info.json
-# mkdir pr_${PR_NUMBER}_related_objects/views
-# mkdir pr_${PR_NUMBER}_related_objects/tables
-# touch pr_${PR_NUMBER}_related_objects/tables/db_object_grant.hql
+#Using the Github API, find the souruce branch of the PR and set it to a variable called PR_BRANCH_NAME
+PR_BRANCH_NAME=$(echo $PR_PROPERTY | jq .head.ref | tr -d '"')
+PR_BRANCH_DESTINATION=$(echo $PR_PROPERTY | jq .base.ref | tr -d '"')
+IS_MERGED=$(echo $PR_PROPERTY | jq .merged | tr -d '"')
 
-# #quickly copy grant file into pr__related_files_folder_tables
-# cp scripts-exec/grant.sql pr_${PR_NUMBER}_related_files_folder_tables
+#if this is a prod deployment, check if the PR has been merged into master
+echo "Deploying into" + DEPLOYMENT_ENV
+echo "This PR is Merging from" + PR_BRANCH_NAME + "into" + PR_BRANCH_DESTINATION
+echo "Is Code Merged already? And:" + IS_MERGED
 
-# #Use Github API to get the current PR properties and Write to the json file created above
-# echo "I will be writing all changes related to PR ${PR_NUMBER} into pr_${PR_NUMBER}_pull_info.json"
-# curl -u "${GITHUB_TOKEN_USR}:${GITHUB_TOKEN_PSW}" -H "Accept: application/vnd.github.v3+json" ${REPO_PULL}${PR_NUMBER}/files >> pr_${PR_NUMBER}_pull_info.json
+if [[ DEPLOYMENT_ENV == "prod" ]]; then
+    if [[ "${PR_BRANCH_DESTINATION}" == "master" ]]; then
+        if [[ "${IS_MERGED}" == "false" ]]; then
+            echo "I cannot Deploy your Code into Prod until it is has successfully merged into the Master Branch"
+            exit -1
+        fi
+    else
+        echo "To Deploy into Prod,the PR must be attempting to Merge into master and not ${PR_BRANCH_DESTINATION}"
+        exit -1
+    fi
+else
+    echo "This will be a Non Prod Deployment"
+fi
+#change into the directory of the dataset repo [It must have been cloned at the inital stage when job was created]
+echo "I will be cloning the dataset repo"
+git clone https://${GITHUB_TOKEN_USR}:${GITHUB_TOKEN_PSW}@github.intuit.com/golokunwolu/${dataset_dir}.git
+cd ${dataset_dir}
 
-# #Using the Github API, find the souruce branch of the PR and set it to a variable called PR_BRANCH_NAME
-# PR_BRANCH_NAME=$(echo $PR_PROPERTY | jq .head.ref | tr -d '"')
-# PR_BRANCH_DESTINATION=$(echo $PR_PROPERTY | jq .base.ref | tr -d '"')
-# IS_MERGED=$(echo $PR_PROPERTY | jq .merged | tr -d '"')
-
-# #if this is a prod deployment, check if the PR has been merged into master
-# echo "Deploying into ${DEPLOYMENT_ENV}"
-# echo "This PR is Merging from ${PR_BRANCH_NAME} into ${PR_BRANCH_DESTINATION}"
-# echo "Is Code Merged already? And: ${IS_MERGED}"
-
-# if [[ "${DEPLOYMENT_ENV}" == "prod" ]]; then
-#     if [[ "${PR_BRANCH_DESTINATION}" == "master" ]]; then
-#         if [[ "${IS_MERGED}" == "false" ]]; then
-#             echo "I cannot Deploy your Code into Prod until it is has successfully merged into the Master Branch"
-#             exit -1
-#         fi
-#     else
-#         echo "To Deploy into Prod,the PR must be attempting to Merge into master and not ${PR_BRANCH_DESTINATION}"
-#         exit -1
-#     fi
-# else
-#     echo "This will be a Non Prod Deployment"
-# fi
-
-# #change into the directory of the dataset repo [It must have been cloned at the inital stage when job was created]
-# echo "I will be cloning the dataset repo"
-# git clone https://${GITHUB_TOKEN_USR}:${GITHUB_TOKEN_PSW}@github.intuit.com/golokunwolu/${dataset_dir}.git
-# cd ${dataset_dir}
-
-# #checkout the PR branch for
-# if [[ "${DEPLOYMENT_ENV}" == "prod" ]]; then
-#     git checkout $PR_BRANCH_DESTINATION
-#     echo "Currently checked into ${PR_BRANCH_DESTINATION} branch"
-# else
-#     git checkout $PR_BRANCH_NAME
-#     echo "Currently checked into ${PR_BRANCH_NAME} branch"
-# fi
+#checkout the PR branch for
+if [[ "${DEPLOYMENT_ENV}" == "prod" ]]; then
+    git checkout $PR_BRANCH_DESTINATION
+    echo "Currently checked into ${PR_BRANCH_DESTINATION} branch"
+else
+    git checkout $PR_BRANCH_NAME
+    echo "Currently checked into ${PR_BRANCH_NAME} branch"
+fi
 
 sql_file=open("artifact_folder/sale.sql")
 sql_as_string=sql_file.read()
